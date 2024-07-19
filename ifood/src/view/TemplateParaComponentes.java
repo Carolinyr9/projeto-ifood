67544347fb2cf951084ccecdfe4dbf1c7d93ec0d
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-19 20:45:52 -0300

    feat: add template

---
+++ ifood/src/view/TemplateParaComponentes.java
@@ -0,0 +1,31 @@
+package view;
+
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class TemplateParaComponentes extends Composite {
+		
+	private LocalResourceManager localResourceManager;
+	private Display display = getDisplay();
+
+    private void createResourceManager() {
+		localResourceManager = new LocalResourceManager(JFaceResources.getResources(), this);
+	}
+
+
+    public TemplateParaComponentes(Composite parent, MainPage mainPage) {
+		super(parent, SWT.NONE);
+		createResourceManager();
+		//Pagrão de largura e altura das telas de componente
+		setSize(468, 774);
+		setLayout(new FormLayout());
+		
+		
+	}
+
+}
