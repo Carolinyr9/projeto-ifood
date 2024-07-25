70fb61a3b17d32c3f5c488ad6fca7c3183342274
Jessybr <jessicabuenoramos5@gmail.com>
2024-07-25 16:51:54 -0300

    feat: create component

---
+++ ifood/src/view/CadCardapio.java
@@ -0,0 +1,125 @@
+package view;
+
+import org.eclipse.jface.resource.ColorDescriptor;
+import org.eclipse.jface.resource.FontDescriptor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.resource.LocalResourceManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class CadCardapio extends Composite {
+		
+	private LocalResourceManager localResourceManager;
+	private Display display = getDisplay();
+	private Text textTituloCardapio;
+
+    private void createResourceManager() {
+		localResourceManager = new LocalResourceManager(JFaceResources.getResources(), this);
+	}
+
+
+    public CadCardapio(Composite parent, MainPage mainPage) {
+		super(parent, SWT.NONE);
+		createResourceManager();
+		setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(255, 255, 255))));
+		setSize(468, 774);
+		setLayout(new FormLayout());
+		
+Image arrowIcon = new Image(display, "./src/assets/images/backArrow.png");
+		
+		Composite compositeHeader = new Composite(this, SWT.NONE);
+		compositeHeader.setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(0, 100, 145))));
+		FormData fd_compositeHeader = new FormData();
+		fd_compositeHeader.top = new FormAttachment(0);
+		fd_compositeHeader.left = new FormAttachment(0);
+		fd_compositeHeader.bottom = new FormAttachment(0, 73);
+		fd_compositeHeader.right = new FormAttachment(0, 468);
+		compositeHeader.setLayoutData(fd_compositeHeader);
+		
+		Label lblCadastrarItem = new Label(compositeHeader, SWT.CENTER);
+		lblCadastrarItem.setAlignment(SWT.CENTER);
+		lblCadastrarItem.setForeground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(232, 241, 242))));
+		lblCadastrarItem.setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(0, 100, 145))));
+		lblCadastrarItem.setFont(localResourceManager.create(FontDescriptor.createFrom("Segoe UI", 16, SWT.NORMAL)));
+		lblCadastrarItem.setBounds(86, 12, 251, 51);
+		lblCadastrarItem.setText("Cadastrar cardapio");
+		
+		Button btnBack = new Button(compositeHeader, SWT.NONE);
+		btnBack.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				mainPage.navigateToScreenEmployee(1);
+			}
+		});
+		btnBack.addPaintListener( new PaintListener() {
+			  @Override
+			  public void paintControl( PaintEvent event ) {
+			    event.gc.setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(0, 100, 148))));
+			    event.gc.fillRectangle( event.x, event.y, event.width, event.height );
+			    event.gc.drawImage(arrowIcon, 0, 0);
+			  }
+			} );
+		btnBack.setBounds(20, 10, 60, 53);
+		
+		Composite composite = new Composite(this, SWT.NONE);
+		composite.setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(255, 255, 255))));
+		composite.setLayout(null);
+		FormData fd_composite = new FormData();
+		fd_composite.bottom = new FormAttachment(compositeHeader, 691, SWT.BOTTOM);
+		fd_composite.top = new FormAttachment(compositeHeader, 6);
+		fd_composite.right = new FormAttachment(compositeHeader, 0, SWT.RIGHT);
+		fd_composite.left = new FormAttachment(0);
+		composite.setLayoutData(fd_composite);
+		
+		Label lblTituloDoCardpio = new Label(composite, SWT.NONE);
+		lblTituloDoCardpio.setBackground(localResourceManager.create(ColorDescriptor.createFrom(new RGB(255, 255, 255))));
+		lblTituloDoCardpio.setBounds(63, 55, 174, 28);
+		lblTituloDoCardpio.setFont(localResourceManager.create(FontDescriptor.createFrom("Segoe UI", 12, SWT.BOLD)));
+		lblTituloDoCardpio.setText("Titulo do cardápio");
+		
+		textTituloCardapio = new Text(composite, SWT.BORDER);
+		textTituloCardapio.setBounds(63, 88, 250, 36);
+		
+		Button btnConcluir = new Button(composite, SWT.NONE);
+		btnConcluir.setFont(localResourceManager.create(FontDescriptor.createFrom("Segoe UI", 11, SWT.NORMAL)));
+		btnConcluir.setBounds(63, 279, 114, 36);
+		btnConcluir.setText("Concluir");
+		btnConcluir.addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				GC gc = e.gc;
+				Rectangle rect = btnConcluir.getBounds();
+				Color blue = new Color(getDisplay(), new RGB(19, 41, 61));
+				Color white = new Color(getDisplay(), new RGB(255, 255, 255));
+				gc.setAntialias(SWT.ON);
+				gc.setBackground(blue);
+				gc.fillRoundRectangle(0, 0, rect.width, rect.height, 40, 40);
+				gc.setForeground(white);
+				gc.setFont(localResourceManager.create(FontDescriptor.createFrom("Segoe UI", 11, SWT.NORMAL)));
+				String text = "Concluir";
+				int textWidth = gc.textExtent(text).x;
+				int textHeight = gc.textExtent(text).y;
+				gc.drawText(text, (rect.width - textWidth) / 2, (rect.height - textHeight) / 2, true);
+
+				blue.dispose();
+				white.dispose();
+			}
+		});
+	}
+}
